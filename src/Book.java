b8d9c599bac37d5c2aa23b26c1e870f20ea87fd0
denise.leon <denise.leon@elev.ga.ntig.se>
2023-12-06 11:11:02 +0100

    adding

--- src/Book.java
+++ src/Book.java
@@ -2,7 +2,21 @@ public class Book {
     int pages;
     Boolean hardCover;
     String genre;
+    int currentPage;
+    double bookState = 1.0;
+    int lendingPeriod = 30;
 
-    public void
+    public Book() {
+    }
+
+    public void placeBookmark(){
+
+    }
+
+
+
+
+
+
 
 }
---
+++ src/Human.java
@@ -0,0 +1,4 @@
+public class Human {
+    String Name;
+    int age;
+}
